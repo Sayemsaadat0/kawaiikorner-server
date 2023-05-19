c0d466cc61f91caf49589650172f598f65fe2239
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-05-20 00:49:15 +0600

    server created and mongodb database added

---
+++ index.js
@@ -0,0 +1,45 @@
+const express=require('express');
+const cors=require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app=express();
+require('dotenv').config();
+
+const port=process.env.PORT || 5000;
+
+app.use(express.json());
+app.use(cors());
+
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.njebycd.mongodb.net/?retryWrites=true&w=majority`
+console.log(uri);
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+
+    
+  } finally {
+    
+  }
+}
+run().catch(error=>console.error(error));
+
+
+app.get('/', (req, res)=>{
+    res.send('server is running')
+});
+
+app.listen(port,()=>{
+console.log(`Server is running on port ${port}`);
+})
