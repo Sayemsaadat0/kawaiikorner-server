26ef4ff6e2f7d2f68ffe07200bfcab5601292a03
Sayemsaadat0 <sayemsaadat0@gmail.com>
2023-05-20 16:03:56 +0600

    all toy

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ const cors=require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const app=express();
 require('dotenv').config();
-
 const port=process.env.PORT || 5000;
+
+
 
 app.use(express.json());
 app.use(cors());
@@ -26,17 +27,64 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     await client.connect();
+
+
     const categoryCollection= client.db('kawaidb').collection('categories')
+    // Add to data toy collection
     const toyCollection= client.db('kawaidb').collection('toy')
 
+
+
+    const subCatName= client.db('kawaidb').collection('subCategoryName')
+    const subcatdetails= client.db('kawaidb').collection('subCategoryDetails')
+
+
+   
+        
+    // post for add  a  toy
     app.post('/addtoy', async(req,res)=>{
-      const data=req.body;
-      const result= await toyCollection.insertOne(data);
+      const addData =req.body;
+      const result= await toyCollection.insertOne(addData);
       res.send(result)
       })
 
 
+   // show all toy to the alltoy page what we addeded in add a toy page
+   app.get('/addtoy', async (req, res)=>{
+    console.log(req.query)
+     let query = {}
+    if(req.query?.email){
+      query = {email: req.query.Seller_email} } 
+    const result = await toyCollection.find(query).toArray()
+    res.send(result)
+          })
     
+
+
+      // sub category 
+      app.get('/subToy', async(req, res)=>{
+        console.log(req.query)
+        const cursor = subCatName.find() 
+        const result = await cursor.toArray()
+        res.send(result)
+      })
+
+      app.get('/subtoy/:name' , async (req , res ) => {
+        const name=req.query.name
+        const query= {name: name}
+        const result =await categoryDetailsCollection.find(query).toArray()
+        res.send(result)
+      })
+
+
+
+
+
+
+
+
+     await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } 
   finally {
     
@@ -46,7 +94,7 @@ run().catch(error=>console.error(error));
 
 
 app.get('/', (req, res)=>{
-    res.send('server is running')
+    res.send('kawai is running at 5000')
 });
 
 app.listen(port,()=>{
